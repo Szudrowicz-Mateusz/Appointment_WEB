f8a745cd825a7a86a4a4e68c0f565ac819eee834
Mus <Mus@DESKTOP-OFRSAEU>
2023-10-27 11:09:31 +0200

    Dodaj pliki projektów.

---
+++ Appointment_WEB/Controllers/AppointmentController.cs
@@ -0,0 +1,45 @@
+﻿using Appointment_WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appointment_WEB.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private List<AppointmentModel> appointments = new List<AppointmentModel>();
+
+        public IActionResult Show()
+        {
+
+            // Dummy data
+            var dummyAppointments = new List<AppointmentModel>
+              {
+                new AppointmentModel(1, "Title1", "Desc1", Day.Thursday, new TimeSpan(10, 0, 0), new TimeSpan(12, 0, 0)),
+                new AppointmentModel(2, "Title2", "Desc2", Day.Monday, new TimeSpan(8, 0, 0), new TimeSpan(15, 0, 0))
+            };
+
+            return View(dummyAppointments);
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AppointmentModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                appointments.Add(model);
+
+                
+                return RedirectToAction("Show");
+            }
+            return View(model);
+        }
+    }
+
+}
---
+++ Appointment_WEB/Models/AppointmentModel.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Appointment_WEB.Models
+{
+    public enum Day
+    {
+        Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+    }
+
+    public class StartTimeEarlierThanEndTimeAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var appointment = (AppointmentModel)validationContext.ObjectInstance;
+
+            if (appointment.startTime >= appointment.endTime)
+            {
+                return new ValidationResult("Start time must be earlier than end time.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+
+    public class AppointmentModel
+    {
+        public AppointmentModel() { }
+        public AppointmentModel(int id, String title, String description, Day day,TimeSpan startTime,TimeSpan endTime) 
+        {
+            this.id = id; this.title = title; this.description = description; this.day = day; this.startTime = startTime; this.endTime = endTime;
+        }
+
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public String title { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        public Day day { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        [StartTimeEarlierThanEndTime]
+        public TimeSpan startTime { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        public TimeSpan endTime { get; set; }
+    }
+}
