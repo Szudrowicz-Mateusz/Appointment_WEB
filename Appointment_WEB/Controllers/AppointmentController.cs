17c35bc7fa25f1e708c7c91bb8c49b995917c20e
Mus <szudrowiczmateusz@gmail.com>
2023-10-29 12:33:16 +0100

    Adding some style to Show.cshtml

--- Appointment_WEB/Controllers/AppointmentController.cs
+++ Appointment_WEB/Controllers/AppointmentController.cs
@@ -14,7 +14,9 @@ namespace Appointment_WEB.Controllers
             var dummyAppointments = new List<AppointmentModel>
               {
                 new AppointmentModel(1, "Title1", "Desc1", Day.Thursday, new TimeSpan(10, 0, 0), new TimeSpan(12, 0, 0)),
-                new AppointmentModel(2, "Title2", "Desc2", Day.Monday, new TimeSpan(8, 0, 0), new TimeSpan(15, 0, 0))
+                new AppointmentModel(2, "Title2", "Desc2", Day.Monday, new TimeSpan(8, 0, 0), new TimeSpan(15, 0, 0)),
+                new AppointmentModel(3, "Tfmsidmnfdsifdmifdmsfdimfdsidmsdf", "Desc3", Day.Friday, new TimeSpan(9, 30, 0), new TimeSpan(10, 45, 0)),
+                new AppointmentModel(3, "Small", "Desc4", Day.Friday, new TimeSpan(11, 0, 0), new TimeSpan(12, 0, 0))
             };
 
             return View(dummyAppointments);
