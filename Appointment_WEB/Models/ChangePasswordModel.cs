cf9aad4cd05783187592a4cf2485741e3479c2df
Mus <szudrowiczmateusz@gmail.com>
2023-11-09 12:47:27 +0100

    Added Profile and changing a password for the user

---
+++ Appointment_WEB/Models/ChangePasswordModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Appointment_WEB.Models
+{
+    public class ChangePasswordModel
+    {
+        [Required]
+        public string OldPassword { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+        [Required]
+        public string RepeatNewPassword { get; set; }
+    }
+}
